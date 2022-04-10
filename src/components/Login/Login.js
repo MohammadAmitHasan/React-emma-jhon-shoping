4c369a2b9eda7b239a24509a795bf32d28fec3f3
Muhammad Amit Hasan <bhamithasan@gmail.com>
2022-04-10 15:17:32 +0600

    Sign up form done

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Login.css'
+import googleLogo from '../../images/google.png'
+
 const Login = () => {
     return (
         <div className='form-container'>
@@ -9,15 +11,32 @@ const Login = () => {
                 <form>
                     <div className="input-group">
                         <label htmlFor="email">Email</label>
-                        <input type="email" name="email" id="" placeholder='Your Email' />
+                        <input type="email" name="email" id="" placeholder='Your Email' required />
                     </div>
                     <div className="input-group">
                         <label htmlFor="password">Password</label>
-                        <input type="email" name="password" id="" placeholder='Type your password' />
+                        <input type="email" name="password" id="" placeholder='Type your password' required />
                     </div>
                     <input className='form-submit-btn' type="submit" value="Login" />
                 </form>
                 <p>Don't have an account.? <Link className='form-toggle' to='/signup'>click to signup</Link> </p>
+
+                <div className='special-separator'>
+                    <div>
+                        <hr />
+                    </div>
+                    <div style={{ textAlign: 'center' }}>
+                        OR
+                    </div>
+                    <div>
+                        <hr />
+                    </div>
+                </div>
+
+                <button className='google-sign-in-btn'>
+                    <img src={googleLogo} alt="" />
+                    <span>Sign In With Google</span>
+                </button>
             </div>
         </div>
     );
--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,10 +1,51 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import googleLogo from '../../images/google.png'
 
 const SignUp = () => {
     return (
-        <div>
-            <h1>SignUp</h1>
+        <div className='form-container'>
+            <div>
+                <h3 className='form-title'>Sign Up</h3>
+                <form>
+                    <div className="input-group">
+                        <label htmlFor="email">Email</label>
+                        <input type="email" name="email" id="" placeholder='Your Email' required />
+                    </div>
+
+                    <div className="input-group">
+                        <label htmlFor="password">Password</label>
+                        <input type="email" name="password" id="" placeholder='Type your password' required />
+                    </div>
+
+                    <div className="input-group">
+                        <label htmlFor="confirm-password">Confirm Password</label>
+                        <input type="email" name="password" id="" placeholder='Type your password again' required />
+                    </div>
+
+                    <input className='form-submit-btn' type="submit" value="Login" />
+                </form>
+                <p>Already have an account.? <Link className='form-toggle' to='/login'>Login</Link> </p>
+
+                <div className='special-separator'>
+                    <div>
+                        <hr />
+                    </div>
+                    <div style={{ textAlign: 'center' }}>
+                        OR
+                    </div>
+                    <div>
+                        <hr />
+                    </div>
+                </div>
+
+                <button className='google-sign-in-btn'>
+                    <img src={googleLogo} alt="" />
+                    <span>Sign In With Google</span>
+                </button>
+            </div>
         </div>
+
     );
 };
 
